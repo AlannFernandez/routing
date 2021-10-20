38e21e9353e9b56ec17d2dd68c0e96f4bd6475a8
AlannFernandez <88353377+AlannFernandez@users.noreply.github.com>
2021-10-20 18:24:21 -0300

    a terminar trazado de rutas

--- .vscode/app.js
+++ .vscode/app.js
@@ -1,16 +1,43 @@
 
     let map = L.map('map').setView([-28.05815970583552,-56.01599463603798], 15);
-    let latLng1 = L.latLng(-28.057201821899295, -56.0190424917164);
-    let latLng2 = L.latLng(-28.05102611059904,-56.0418487786592);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(mostrarUbicacion);
+    }
+
+
+    function mostrarUbicacion (ubicacion) {
+        const lng = ubicacion.coords.longitude;
+        const lat = ubicacion.coords.latitude;        
+        usuario = L.marker([lat, lng]).bindPopup('estas aca');
+        map.addLayer(usuario);        
+    }
+   
+
+    //user marker
+    map.on('dblclick', e=>{        
+    let destino = null;
+    let latlng = map.mouseEventToLatLng(e.originalEvent);
+    console.log(latlng);
+    destino = L.marker([latlng.lat, latlng.lng]).bindPopup('Alan');
+        if(destino){
+        map.removeLayer(destino);
+        }
+    map.addLayer(destino);
+
+    })
+
+   
+
+    let latLng1 = L.latLng(-28.048525027214637, -56.00477206680808);
+    let latLng2 = L.latLng(-28.04331520533566, -56.018213152656244);
     let wp1 = new L.Routing.Waypoint(latLng1);
-    let wp2 = new L.Routing.Waypoint(latLng2);
+    let wp2 = new L.Routing.Waypoint(latLng2);   
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
     L.Routing.control({
         waypoints: [latLng1,latLng2],
-        language: 'es', 
-        
+        language: 'es'       
     }).addTo(map);
 
     let routeUs = L.Routing.osrmv1();
@@ -30,16 +57,9 @@
             const totalKm =best/1000;
             const to = parseInt(totalKm, 10)
             const price = 200*to;
-            alert(to);
-            alert("precio a pagar es $"+ price);
-            L.Routing.line(routes[bestRoute],{
-                styles : [
-                    {
-                        color : 'red',
-                        weight : '5'
-                    }
-                ]
-            }).addTo(map);
+            // alert("hay "+to+" km");
+            // alert("precio a pagar es $"+ price);
+            L.Routing.line(routes[bestRoute]).addTo(map);
         
         }
 
